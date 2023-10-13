cb1061eaa0a30a313b80751bf171fb1c34afcf4f
Baboo81 <magicbaboo@gmail.com>
2023-10-13 10:41:35 +0200

    Added navbar, footer

--- latienda/src/app/app.component.ts
+++ latienda/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+//Décorateur:
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',//Sélectionne la balise <app-root>
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+//export permet d'exporter le code vers le html
 export class AppComponent {
   title = 'latienda';
 }
